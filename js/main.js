a3f6bd8da5fc194e346600033059a96de67a5e6b
thenewbeginnings <ndong.peter7@gmail.com>
2026-01-05 22:45:34 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,21 @@ toggle.addEventListener("click", () => {
   nav.classList.toggle("active");
   toggle.textContent = nav.classList.contains("active") ? "✖" : "☰";
 });
+// SCROLL REVEAL
+const reveals = document.querySelectorAll(".reveal, .reveal-img");
+
+function revealOnScroll() {
+  const windowHeight = window.innerHeight;
+
+  reveals.forEach((el) => {
+    const elementTop = el.getBoundingClientRect().top;
+    const revealPoint = 100;
+
+    if (elementTop < windowHeight - revealPoint) {
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
